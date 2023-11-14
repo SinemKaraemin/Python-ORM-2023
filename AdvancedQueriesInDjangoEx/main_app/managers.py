162faf500e8fb68d0b864165aa9871955f86f261
Sinem Karaemin <sinemzkaraemin@gmail.com>
2023-11-14 17:48:58 +0200

    Added more solutions

---
+++ AdvancedQueriesInDjangoEx/main_app/managers.py
@@ -0,0 +1,40 @@
+from _decimal import Decimal
+from django.db import models
+from django.db.models import Count, Avg, Min, Max
+
+
+class RealEstateListingManager(models.Manager):
+    def by_property_type(self, property_type: str):
+        return self.filter(property_type=property_type)
+
+    def in_price_range(self, min_price: Decimal, max_price: Decimal):
+        return self.filter(price__range=(min_price, max_price))
+
+    def with_bedrooms(self, bedrooms_count: int):
+        return self.filter(bedrooms=bedrooms_count)
+
+    def popular_locations(self):
+        return (
+            self.values('location')
+            .annotate(visit_count=Count('id'))
+            .order_by('location', '-visit_count')[:2]
+        )
+
+
+class VideoGameManager(models.Manager):
+    def games_by_genre(self, genre: str):
+        return self.filter(genre=genre)
+
+    def recently_released_games(self, year: int):
+        return self.filter(release_year__gte=year)
+
+    def highest_rated_game(self):
+        return self.annotate(max_rating=Max('rating')).order_by('max_rating').first()
+
+    def lowest_rated_game(self):
+        return self.annotate(min_rating=Min('rating')).order_by('min_rating').first()
+
+    def average_rating(self):
+        average_rating = self.aggregate(average_rating=Avg('rating'))['average_rating']
+        return f"{average_rating:.1f}"
+
